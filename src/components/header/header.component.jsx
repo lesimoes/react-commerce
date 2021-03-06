cc3658787f5f2b683d690f670747a9c97d799de3
lesimoes <lesimoes0@gmail.com>
2021-03-06 18:47:22 -0300

    added sign in with google and firebase

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = () => {
+const Header = ({ currentUser }) => {
 
     return (
         <div className='header'>
@@ -15,9 +16,14 @@ const Header = () => {
                 <Link className='option' to='/shop'>
                     SHOP
                 </Link>
-                <Link className='option' to='/shop'>
+                <Link className='option' to='/contact'>
                     CONTACT
                 </Link>
+                { currentUser ? 
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className='option' to='/signin'>SIGN IN</Link>
+                }
             </div>
         </div>
 
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth'
+
+console.log(process.env)
+
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID
+  }
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
